c60523a79fc4ffb65516ddcfb925d64aa39c937d
Avinash175 <avinashmagar15@gmail.com>
2026-04-29 13:04:16 +0530

    refactor: move health method channel initialization after plugin registration in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,27 +8,29 @@ import HealthKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let healthChannel = FlutterMethodChannel(name: "health_channel",
-                                              binaryMessenger: controller.binaryMessenger)
-    
-    healthChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      switch call.method {
-      case "requestPermissions":
-        // Simulate HealthKit permission
-        result(true)
-      case "getSteps":
-        // Simulate HealthKit steps
-        result(8742)
-      case "getSleepData":
-        result(8.2)
-      default:
-        result(FlutterMethodNotImplemented)
-      }
-    })
-
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    if let controller = window?.rootViewController as? FlutterViewController {
+      let healthChannel = FlutterMethodChannel(name: "health_channel",
+                                                binaryMessenger: controller.binaryMessenger)
+      
+      healthChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        switch call.method {
+        case "requestPermissions":
+          result(true)
+        case "getSteps":
+          result(8742)
+        case "getSleepData":
+          result(8.2)
+        default:
+          result(FlutterMethodNotImplemented)
+        }
+      })
+    }
+    
+    return result
   }
 }
